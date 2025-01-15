69b6c518049609ec7ae2574f09c7f0b1a63566d0
Pablo Vallejo <admin@dfbubbles.com>
2025-01-15 12:25:52 +0100

    vercel speed insights

--- deepfriend/src/app/layout.tsx
+++ deepfriend/src/app/layout.tsx
@@ -7,6 +7,7 @@ import BasicRouter from "./basic.router";
 import { Head } from "@/components/head";
 
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const BASIC_DARK_COLOR = "#031521";
 export const BASIC_LIGHT_COLOR = "#F8F8F8";
@@ -24,6 +25,7 @@ export default function RootLayout({
         <body>
           <BasicRouter children={children} />
           <Analytics />
+          <SpeedInsights />
         </body>
       </html>
     </Provider>
