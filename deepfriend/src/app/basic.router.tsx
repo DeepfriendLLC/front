3838c17939fc77f37d9633b29e58f6570aec5028
Pablo Vallejo <admin@dfbubbles.com>
2025-06-23 11:43:33 +0700

    stable version

--- deepfriend/src/app/basic.router.tsx
+++ deepfriend/src/app/basic.router.tsx
@@ -40,7 +40,7 @@ export default function BasicRouter({ children }: { children: React.ReactNode })
 
       setCookie('systemLanguage', initialSystemLanguage);
       dispatch(setSystemLanguageStore(initialSystemLanguage));
-    } else dispatch(setSystemLanguageStore("en"));
+    } else dispatch(setSystemLanguageStore(cookies.systemLanguage));
   };
 
   const updateInitialSystemColor = () => {
