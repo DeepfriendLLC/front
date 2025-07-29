4d08b946455964d42a6ddf4040bd65a207edbd5d
Pablo Vallejo <admin@dfbubbles.com>
2025-07-29 17:36:17 +0700

    stable version metrics

--- deepfriend/src/api/api.ts
+++ deepfriend/src/api/api.ts
@@ -1,9 +1,9 @@
-const baseURL = `https://api.dfbubbles.com/`;
+const baseURL = `https://api.dfbubbles.com`;
 const WEB_API_KEY = `7bc1bf04-56f3-4321-a867-ce90e2873961`;
 
 export async function CreateStatView(route: string, userAgent: string, language: string) {
   try {
-    await fetch(baseURL, {
+    await fetch(`${baseURL}/789cfa081959/create/view`, {
       method: "POST",
       headers: {
         'apikey': WEB_API_KEY,
