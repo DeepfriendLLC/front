b8b97017f8af1e03541bb0251693d56f47170896
Pablo Vallejo <admin@dfbubbles.com>
2025-08-12 00:30:36 +0700

    stable version

--- deepfriend/src/app/page.tsx
+++ deepfriend/src/app/page.tsx
@@ -20,10 +20,12 @@ import HomeProperties9Component from "@/components/home-properties-9";
 import HomeProperties10Component from "@/components/home-properties-10";
 import HomeProperties11Component from "@/components/home-properties-11";
 import VisibleTracker from "@/components/visible-tracker";
+import { SendMetricsSessionFocusAPI } from "@/components/client-api/client-api";
 
 export default function Home() {
   const { systemLanguage } = useSelector((state: RootState) => state.systemLanguage);
   const { systemColor } = useSelector((state: RootState) => state.systemColor);
+  const { sessionId } = useSelector((state: RootState) => state.sessionId);
 
   const containerStyle: CSSProperties = {
     flex: 1,
@@ -31,26 +33,35 @@ export default function Home() {
     flexDirection: "column",
   };
 
-  const focus = () => console.log("EEPAAA FOCUSED");
+  const focus = async (section: string) => {
+    await SendMetricsSessionFocusAPI(sessionId, "/", section, systemColor, systemLanguage);
+  };
 
   return (
     <div style={containerStyle}>
       <HomeBannerComponent />
       <HomeProperties0Component />
+      <VisibleTracker onVisible={async () => await focus("1")} />
       <HomeProperties1Component />
-      <VisibleTracker onVisible={focus} />
+      <VisibleTracker onVisible={async () => await focus("2")} />
       <HomeProperties9Component />
+      <VisibleTracker onVisible={async () => await focus("3")} />
       <HomeProperties8Component />
+      <VisibleTracker onVisible={async () => await focus("4")} />
       <HomeProperties10Component />
       <h1 id="🐳" className="home-properties-4-title" style={{ color: systemColor === "light" ? BASIC_DARK_COLOR : BASIC_LIGHT_COLOR }}>
         {TranslationTexts[systemLanguage].home_properties_9_title_big}
       </h1>
+      <VisibleTracker onVisible={async () => await focus("5")} />
       <HomeProperties4Component />
+      <VisibleTracker onVisible={async () => await focus("6")} />
       <HomeProperties2Component />
+      <VisibleTracker onVisible={async () => await focus("7")} />
       <div className="home-properties-3-wrapped">
         <HomeProperties30Component />
         <HomeProperties31Component />
       </div>
+      <VisibleTracker onVisible={async () => await focus("8")} />
       <h1 className="home-properties-5-title text-center" style={{ color: systemColor === "light" ? BASIC_DARK_COLOR : BASIC_LIGHT_COLOR }}>
         {TranslationTexts[systemLanguage].home_properties_5_title}
       </h1>
@@ -58,7 +69,9 @@ export default function Home() {
         {TranslationTexts[systemLanguage].home_properties_5_subtitle}
       </h1>
       <HomeProperties5Component />
-      <HomeProperties7Component/>
+      <VisibleTracker onVisible={async () => await focus("9")} />
+      <HomeProperties7Component />
+      <VisibleTracker onVisible={async () => await focus("10")} />
       <HomeProperties11Component />
       {
         /*
