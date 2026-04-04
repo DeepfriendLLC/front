f0ded1f9ca2b3423ef9fd57f62ff58827427f708
Pablo Vallejo <admin@dfbubbles.com>
2026-04-04 17:23:38 +0700

    feat: good badge

--- deepfriend/src/components/home/properties/properties-5.tsx
+++ deepfriend/src/components/home/properties/properties-5.tsx
@@ -5,9 +5,17 @@ import styles from "@/styles/Properties.module.css";
 import { useSelector } from "react-redux";
 import { RootState } from "@/hooks/store";
 import { TranslationTexts } from "@/constants/translations/translations";
+import { useWindowWidth } from "@/hooks/width/width";
+
+import PcBackground from "@/../public/images/properties-badge/pc.png";
+import PhoneBackground from "@/../public/images/properties-badge/phone.png";
+
+import { CSSProperties } from "react";
 
 export default function HomeProperties5Component() {
   const { systemLanguage } = useSelector((state: RootState) => state.systemLanguage);
+
+  const width = useWindowWidth();
 
   const googlePlayLink = "https://play.google.com/store/apps/details?id=com.dfbubbles.deepfriend";
 
@@ -15,8 +23,15 @@ export default function HomeProperties5Component() {
     window.open(googlePlayLink, "_blank");
   };
 
+  const cardStyle: CSSProperties = {
+    backgroundImage: `url(${width >= 1000 ? PcBackground.src : PhoneBackground.src})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <div className={styles.badgeCard}>
+    <div className={styles.badgeCard} style={cardStyle}>
       <h1 className={styles.badgeTitle}>
         {TranslationTexts[systemLanguage].home_properties_5_title}
       </h1>
