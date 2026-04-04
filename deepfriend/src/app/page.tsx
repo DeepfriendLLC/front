bd588a12edba2ae4cfa367e3b345dd449c62d867
Pablo Vallejo <admin@dfbubbles.com>
2026-04-04 16:31:02 +0700

    feat: correct spacing phone

--- deepfriend/src/app/page.tsx
+++ deepfriend/src/app/page.tsx
@@ -2,13 +2,13 @@
 
 import styles from "@/styles/global.module.css";
 
+import { NavbarComponent } from "@/components/basic/navbar";
+import { FooterComponent } from "@/components/basic/footer";
+
 import PortadaComponent from "@/components/home/portada/portada";
 import HomeProperties1Component from "@/components/home/properties/properties-1";
 import HomeProperties2Component from "@/components/home/properties/properties-2";
 import HomeProperties3Component from "@/components/home/properties/properties-3";
-import MissionAndValuesComponent from "@/components/home/missionAndValues/mission-and-values";
-import { NavbarComponent } from "@/components/basic/navbar";
-import { FooterComponent } from "@/components/basic/footer";
 import HomeProperties4Component from "@/components/home/properties/properties-4";
 import HomeProperties5Component from "@/components/home/properties/properties-5";
 
@@ -22,7 +22,6 @@ export default function Home() {
       <HomeProperties3Component />
       <HomeProperties4Component />
       <HomeProperties5Component />
-      <MissionAndValuesComponent />
       <FooterComponent />
     </div>
   );
