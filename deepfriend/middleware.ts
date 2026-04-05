e723459511c87dc97ed21ba460b5d63fdac2f02f
Pablo Vallejo <admin@dfbubbles.com>
2026-04-05 11:49:22 +0700

    fix: correct google host

--- deepfriend/middleware.ts
+++ deepfriend/middleware.ts
@@ -17,7 +17,7 @@ export function middleware(request: NextRequest) {
   'Content-Security-Policy',
   "default-src 'self'; " +
   "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
-  "style-src 'self' 'unsafe-inline' https://googleapis.com https://jsdelivr.net; " +
+  "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdn.jsdelivr.net; " +
   "font-src 'self' https://gstatic.com; " + 
   "img-src 'self' data: https:;"
 );
