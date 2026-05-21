3f68cd6a37d1f92c9dd95f442b239cfe65027489
Pablo Vallejo <admin@dfbubbles.com>
2026-05-21 16:24:52 +0700

    chore: [Cursor design refactor commit] removed old carousel and dead code

--- deepfriend/src/constants/general.ts
+++
@@ -1,6 +0,0 @@
-export const BASIC_HORIZONTAL_PADDING = "10vw";
-
-
-export const MINDFULNESS_PALETTE = ["#afeeee", "#24fcb4", "#008b8b", "#f5deb3"];
-export const CICLES_PALETTE = ["#6b2c5f", "#8b3a8b", "#a855a8", "#c77dc7", "#e5b3e5"];
-export const ZENVERSE_PALETTE = ["#F6F4F1", "#e6dbce", "#A79A87"];
