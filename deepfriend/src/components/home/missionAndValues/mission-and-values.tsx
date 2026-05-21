6f99b3d2cfa9fea8771be94e340035ff5e42afac
Pablo Vallejo <admin@dfbubbles.com>
2026-05-21 16:25:56 +0700

    feat: [Cursor design refactor commit] refreshed mission and values block

--- deepfriend/src/components/home/missionAndValues/mission-and-values.tsx
+++ deepfriend/src/components/home/missionAndValues/mission-and-values.tsx
@@ -4,25 +4,22 @@ import { TranslationTexts } from "@/constants/translations/translations";
 import { Locale } from "@/i18n/config";
 
 export default function MissionAndValuesComponent({ lang }: { lang: Locale }) {
+  const t = TranslationTexts[lang];
 
   return (
-    <div className={styles['container']}>
-      <div className={styles['textContainer']}>
-        <h1 className={styles['title']}>
-          {TranslationTexts[lang]!['home_mission_title']}
-        </h1>
-        <h1 className={styles['text']}>
-          {TranslationTexts[lang]!['home_mission_text']}
-        </h1>
+    <section className={styles['section']}>
+      <div className={`df-shell ${styles['shell']}`}>
+        <article className={styles['card']}>
+          <span className={styles['kicker']}>01</span>
+          <h3 className={styles['title']}>{t.about_mission_title}</h3>
+          <p className={styles['text']}>{t.about_mission_text}</p>
+        </article>
+        <article className={styles['card']}>
+          <span className={styles['kicker']}>02</span>
+          <h3 className={styles['title']}>{t.about_values_title}</h3>
+          <p className={styles['text']}>{t.about_values_text}</p>
+        </article>
       </div>
-      <div className={styles['textContainer']}>
-        <h1 className={styles['title']}>
-          {TranslationTexts[lang]!['home_values_title']}
-        </h1>
-        <h1 className={styles['text']}>
-          {TranslationTexts[lang]!['home_values_text']}
-        </h1>
-      </div>
-    </div>
+    </section>
   );
 }
