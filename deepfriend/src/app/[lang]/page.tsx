7dab4a6a9ce256932f00edcae780c0026bdfc9ac
Pablo Vallejo <admin@dfbubbles.com>
2026-05-21 16:25:58 +0700

    feat: [Cursor design refactor commit] wired new home page layout

--- deepfriend/src/app/[lang]/page.tsx
+++ deepfriend/src/app/[lang]/page.tsx
@@ -1,36 +1,35 @@
 import "@/styles/global.css";
 
-import NavbarComponent  from "@/components/basic/navbar";
+import NavbarComponent from "@/components/basic/navbar";
 import FooterComponent from "@/components/basic/footer";
 
 import PortadaComponent from "@/components/home/portada/portada";
-import HomeProperties1Component from "@/components/home/properties/properties-1";
-import HomeProperties2Component from "@/components/home/properties/properties-2";
-import HomeProperties3Component from "@/components/home/properties/properties-3";
-import HomeProperties4Component from "@/components/home/properties/properties-4";
-import HomeProperties5Component from "@/components/home/properties/properties-5";
-import HomeProperties7Component from "@/components/home/properties/properties-7";
-import HomeProperties6Component from "@/components/home/properties/properties-6";
-import HomeProperties8Component from "@/components/home/properties/properties-8";
-import HomePropertiesLinksComponent from "@/components/home/properties/properties-links";
-import { Locale } from "@/i18n/config";
+import TrustComponent from "@/components/home/trust/trust";
+import ApproachComponent from "@/components/home/approach/approach";
+import ProductsComponent from "@/components/home/products/products";
+import PrivacyComponent from "@/components/home/privacy/privacy";
+import CtaComponent from "@/components/home/cta/cta";
 
-export default async function Home({ params }: { params: Promise<{ lang: Locale }> }) {
-  const { lang } = await params;
+import { pickLocale } from "@/i18n/config";
+
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ lang: string }>;
+}) {
+  const lang = pickLocale((await params).lang);
 
   return (
     <>
-      <NavbarComponent />
-      <PortadaComponent lang={lang} />
-      <HomeProperties1Component lang={lang} />
-      <HomeProperties2Component lang={lang} />
-      <HomeProperties3Component lang={lang} />
-      <HomeProperties4Component lang={lang} />
-      <HomeProperties5Component lang={lang} />
-      <HomeProperties6Component lang={lang} />
-      <HomeProperties7Component lang={lang} />
-      <HomeProperties8Component lang={lang} />
-      <HomePropertiesLinksComponent lang={lang} />
+      <NavbarComponent lang={lang} />
+      <main>
+        <PortadaComponent lang={lang} />
+        <TrustComponent lang={lang} />
+        <ApproachComponent lang={lang} />
+        <ProductsComponent lang={lang} />
+        <PrivacyComponent lang={lang} />
+        <CtaComponent lang={lang} />
+      </main>
       <FooterComponent lang={lang} />
     </>
   );
